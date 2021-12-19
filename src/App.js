60d5a87be478a70bbb0099887797e24c7119365b
atul-mandavkar <76526334+atul-mandavkar@users.noreply.github.com>
2021-12-19 21:36:26 +0530

    Usig use state
    But incomplete css styling for mobile view

--- src/App.js
+++ src/App.js
@@ -14,22 +14,66 @@ import TimerNumber from "./components/TimerNumber";
 import ButtonPlay from "./components/ButtonPlay";
 import ButtonPause from "./components/ButtonPause";
 import ButtonReset from "./components/ButtonReset";
+import React, {useState} from "react";
+
+const d = new Date();
 
 function App() {
+  
+  let [tValue, setTValue] = useState({
+    sessionT: 25,
+    breakT: 5,
+    timeVal: "25:00"
+  });
+  const incSession = () => {
+    setTValue({
+      ...tValue,
+      sessionT:
+        tValue.sessionT === 59
+        ? 0
+        : Number(tValue.sessionT)+1
+    });
+  };
+  const decSession = () => {
+    setTValue({
+      ...tValue,
+      sessionT:
+        tValue.sessionT === 0
+        ? 59
+        : Number(tValue.sessionT)-1
+    });
+  };
+  const incBreak = () => {
+    if(tValue.breakT < 60){
+      setTValue({
+        ...tValue,
+        breakT: Number(tValue.breakT)+1
+      });
+    }
+  };
+  const decBreak = () => {
+    if(tValue.breakT > 1){
+      setTValue({
+        ...tValue,
+        breakT: Number(tValue.breakT)-1
+      });
+    }
+  };
+
   return (
     <Wrapper>
       <h1>25 + 5 clock</h1>
       <Wrapper2>
         <LeftSide>
-          <Arrow className="fas fa-arrow-down" onClick=""></Arrow>
-          <BreakTime></BreakTime>
-          <Arrow className="fas fa-arrow-up" onClick=""></Arrow>
+          <Arrow className="fas fa-arrow-down" onClick={decBreak}></Arrow>
+          <BreakTime value={tValue.breakT}></BreakTime>
+          <Arrow className="fas fa-arrow-up" onClick={incBreak}></Arrow>
         </LeftSide>
         <MiddleSide>
           <TimerClock>
             <TimerScreen>
               <TimerHeading value="session"></TimerHeading>
-              <TimerNumber value="00:00"></TimerNumber>
+              <TimerNumber value={tValue.timeVal}></TimerNumber>
             </TimerScreen>
             <TimerButton>
               <ButtonPlay></ButtonPlay>
@@ -39,9 +83,9 @@ function App() {
           </TimerClock>
         </MiddleSide>
         <RightSide>
-          <Arrow className="fas fa-arrow-down" onClick=""></Arrow>
-          <SessionTime></SessionTime>
-          <Arrow className="fas fa-arrow-up" onClick=""></Arrow>
+          <Arrow className="fas fa-arrow-down" onClick={decSession}></Arrow>
+          <SessionTime value={tValue.sessionT}></SessionTime>
+          <Arrow className="fas fa-arrow-up" onClick={incSession}></Arrow>
         </RightSide>
       </Wrapper2>
     </Wrapper>
